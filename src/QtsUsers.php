fea9a023d4a7493385716fc800a164466c15e012
Marcelo Motta - MDM <marcelo@mdm.com.br>
2022-04-09 17:05:03 -0300

    Update QtsUsers.php

--- src/QtsUsers.php
+++ src/QtsUsers.php
@@ -31,4 +31,38 @@ class QtsUsers
 
     }
 
+    public static function cachedUsers($env='LOCAL', $headers)
+    {
+
+        switch(strtoupper($env)){
+            case 'LOCAL': $endpoint = 'http://local-users.quatainvestimentos.com.br:4001/api/'; break;
+            case 'TESTING': $endpoint = 'https://dev-users.quatainvestimentos.com.br/api/'; break;
+            case 'PRODUCTION': $endpoint = 'http://users.quatainvestimentos.com.br/api/'; break;
+            default: echo 'Endpoint de alertas desconhecido: ' . $env; exit;
+        }
+
+        $results = QtsApi::fetch($endpoint . 'cache/users', 'GET', $headers);
+        $data = (isset($results->data) && $results->data ? $results->data : []);
+
+        if(isset($results->status) && $results->status !== 200){
+            return (object)['status' => 404, 'data' => $results->data];
+        }
+
+        return (object)['status' => 200, 'data' => $data->results];
+
+    }
+
+    public static function filterPerson($key, $object)
+    {
+        foreach($object as $person):
+            if($person->api_key === $key){
+                return $person;
+                break;
+            }
+        endforeach;
+
+        return (object)[];
+
+    }
+
 }
