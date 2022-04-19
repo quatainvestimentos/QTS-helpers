cad42b8379abd30d6e395c20539d280ab382da76
Marcelo Motta - MDM <marcelo@mdm.com.br>
2022-04-19 15:21:30 -0300

    Update QtsHelpers.php

--- src/QtsHelpers.php
+++ src/QtsHelpers.php
@@ -210,4 +210,131 @@ class QtsHelpers
         return $value;
     }
 
+    public static function translate_month($int){
+        switch($int){
+            case 1: return 'janeiro'; break;
+            case 2: return 'fevereiro'; break;
+            case 3: return 'março'; break;
+            case 4: return 'abril'; break;
+            case 5: return 'maio'; break;
+            case 6: return 'junho'; break;
+            case 7: return 'julho'; break;
+            case 8: return 'agosto'; break;
+            case 9: return 'setembro'; break;
+            case 10: return 'outubro'; break;
+            case 11: return 'novembro'; break;
+            case 12: return 'dezembro'; break;
+            default: return 'desconhecido: ' . $int;
+        }
+    }
+
+    public static function translate_weekday($int){
+        switch($int){
+            case 0: return 'segunda feira'; break;
+            case 1: return 'terça feira'; break;
+            case 2: return 'quarta feira'; break;
+            case 3: return 'quinta feira'; break;
+            case 4: return 'sexta feira'; break;
+            case 5: return 'sábado'; break;
+            case 6: return 'domingo'; break;
+            default: return 'desconhecido: ' . $int;
+        }
+    }
+
+    public static function translate_crons($value){
+        list($minute,$hour,$month_day,$month,$weekday) = explode(' ', $value);
+        
+        if($minute === '*'){
+            
+            $minute = 'De minuto em minuto';
+       
+        } else {
+
+            if(strpos($minute,'*/') !== false ){
+                list($every,$minute) = explode('/', $minute);
+                $minute = "A cada {$minute} minuto(s)";
+            }
+
+            if(strpos($minute,'-') !== false ){
+                list($from,$to) = explode('-', $minute);
+                $minute = "Todo minuto no intervalo de {$from} a {$to} minuto(s)";
+            }
+
+        }
+
+        if($hour === '*'){
+
+            $hour = 'de hora em hora,';
+
+        } else {
+
+            if(strpos($hour,'*/') !== false ){
+                list($every,$hour) = explode('/', $hour);
+                $hour = "às {$hour} hora(s),";
+            }
+
+            if(strpos($hour,'-') !== false ){
+                list($from,$to) = explode('-', $hour);
+                $hour = "das {$from} a {$to} hora(s),";
+            }
+
+        }
+
+        if($month_day === '*'){
+
+            $month_day = 'todos os dias,';
+
+        } else {
+
+            if(strpos($month_day,'*/') !== false ){
+                list($every,$month_day) = explode('/', $month_day);
+                $month_day = "somente no {$month_day}º dia do mês,";
+            }
+
+            if(strpos($month_day,'-') !== false ){
+                list($from,$to) = explode('-', $month_day);
+                $month_day = "do {$from}º ao {$to}º dia do mês,";
+            }
+
+        }
+
+        if($month === '*'){
+
+            $month = 'o ano todo';
+
+        } else {
+
+            if(strpos($month,'*/') !== false ){
+                list($every,$month) = explode('/', $month);
+                $month = "somente no mês de " . QtsHelpers::translate_month($month);
+            }
+
+            if(strpos($month,'-') !== false ){
+                list($from,$to) = explode('-', $month);
+                $month = "de " . QtsHelpers::translate_month($from) . " a " . QtsHelpers::translate_month($to);
+            }
+
+        }
+
+        if($weekday === '*'){
+
+            $weekday = 'e em todos os dias da semana';
+
+        } else {
+
+            if(strpos($weekday,'*/') !== false ){
+                list($every,$weekday) = explode('/', $weekday);
+                $weekday = "somente de " . QtsHelpers::translate_weekday($weekday);
+            }
+
+            if(strpos($weekday,'-') !== false ){
+                list($from,$to) = explode('-', $weekday);
+                $weekday = "no intervalo de " . QtsHelpers::translate_weekday($from) . " a " . QtsHelpers::translate_weekday($to);
+            }
+
+        }
+
+        return "{$minute} {$hour} {$month_day} {$month} {$weekday}.";
+    }
+
 }
