173047a3a1d2591496fa3cc34b17e0c08643dec6
Marcelo Motta - MDM <marcelo@mdm.com.br>
2022-09-22 12:24:45 -0300

    Update Discharge.php

--- src/Bank/Bradesco/Discharge.php
+++ src/Bank/Bradesco/Discharge.php
@@ -5,7 +5,7 @@ namespace QuataInvestimentos\Bank\Bradesco;
 use QuataInvestimentos\Bank\Common;
 use QuataInvestimentos\Bank\Bradesco\Discharge\Init;
 use QuataInvestimentos\Bank\Bradesco\Discharge\Header;
-use QuataInvestimentos\Bank\Bradesco\Discharge\Transaction2;
+use QuataInvestimentos\Bank\Bradesco\Discharge\Transaction1;
 use QuataInvestimentos\Bank\Bradesco\Discharge\Transaction3;
 use QuataInvestimentos\Bank\Bradesco\Discharge\Footer;
 
