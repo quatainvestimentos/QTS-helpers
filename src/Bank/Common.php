e7384f2753215aeae1e81b0a5df402c1339ab369
Marcelo Motta - MDM <marcelo@mdm.com.br>
2022-09-29 15:51:10 -0300

    Update Common.php

--- src/Bank/Common.php
+++ src/Bank/Common.php
@@ -48,8 +48,12 @@ trait Common {
         
         $string = preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/","/(ç)/","/(Ç)/"),explode(" ","a A e E i I o O u U n N c C"),$string);
         $string = preg_replace("/[^A-Za-z0-9 ]-/", ' ', $string);
-        $string = str_replace(array("\\", "/"), '.', $string);
+        $string = str_replace(array("\\"), '.', $string);
         $string = str_replace('?',' ', $string);
+        $string = str_replace('\/','--', $string);
+        # Não podemos remover / porque o sdocumento (titulo)
+        # pode conter barras
+        # $string = str_replace('/',' ', $string);
         $string = str_replace(array("\n", "\r"), '', $string);
         $string = strtoupper($string);
 
